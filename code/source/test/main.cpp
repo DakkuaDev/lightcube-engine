c507689bbb27e8f4a7bf9920e905bdf1bc9606b2
Daniel Guerra <wordgames01@gmail.com>
2020-12-12 00:53:27 +0100

    re-structured project folders and solutions. sdl2 and gtest re-imported individually

---
+++ code/source/test/main.cpp
@@ -0,0 +1,27 @@
+/**
+ * Copyright (C) Daniel Guerra Gallardo
+ *
+ * @section LICENSE MIT License (see LICENSE file for more information)
+ *
+ * @section DESCRIPTION
+ * This is LightCube Engine, a personal game engine for education purpouses.
+ *
+ * @author Daniel Guerra Gallardo
+ * Contact: daniel.guerra.gallardo99@gmail.com
+ *
+ */
+
+#include <gtest/gtest.h>
+
+int main()
+{
+	return 0;
+}
+
+namespace myTest
+{
+	TEST(initialTest, subTest)
+	{
+		ASSERT_TRUE(1 == 1);
+	}
+}
---
+++ code/source/window.cpp
@@ -0,0 +1,14 @@
+/**
+ * Copyright (C) Daniel Guerra Gallardo
+ *
+ * @section LICENSE MIT License (see LICENSE file for more information)
+ * 
+ * @section DESCRIPTION
+ * This is LightCube Engine, a personal game engine for education purpouses.
+ * 
+ * @author Daniel Guerra Gallardo
+ * Contact: daniel.guerra.gallardo99@gmail.com
+ *
+ */
+
+#include <SDL.h>
